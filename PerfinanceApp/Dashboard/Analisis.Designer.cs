f14555c1ef8fe510320cf32ee4a32fa7f6521708
DanielMirelesS <danielwtfxd@hotmail.com>
2022-10-30 23:37:00 -0600

    visual graficas

--- PerfinanceApp/Dashboard/Analisis.Designer.cs
+++ PerfinanceApp/Dashboard/Analisis.Designer.cs
@@ -37,48 +37,116 @@
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.grafic1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.graficpastel = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.grafic1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.graficpastel)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // grafic1
             // 
+            this.grafic1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            chartArea1.AxisX.IsMarginVisible = false;
+            chartArea1.AxisX.LabelStyle.ForeColor = System.Drawing.Color.Silver;
+            chartArea1.AxisX.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX.LineWidth = 0;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX.MajorGrid.LineWidth = 0;
+            chartArea1.AxisX.MajorTickMark.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(75)))), ((int)(((byte)(111)))));
+            chartArea1.AxisX.MajorTickMark.Size = 3F;
+            chartArea1.AxisY.LabelStyle.ForeColor = System.Drawing.Color.Silver;
+            chartArea1.AxisY.LabelStyle.Format = "${0}";
+            chartArea1.AxisY.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisY.LineWidth = 0;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(75)))), ((int)(((byte)(111)))));
+            chartArea1.AxisY.MajorTickMark.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(75)))), ((int)(((byte)(111)))));
+            chartArea1.AxisY.MajorTickMark.LineWidth = 0;
+            chartArea1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
             chartArea1.Name = "ChartArea1";
             this.grafic1.ChartAreas.Add(chartArea1);
+            legend1.BackColor = System.Drawing.Color.Transparent;
+            legend1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            legend1.ForeColor = System.Drawing.Color.Gainsboro;
+            legend1.IsTextAutoFit = false;
             legend1.Name = "Legend1";
             this.grafic1.Legends.Add(legend1);
-            this.grafic1.Location = new System.Drawing.Point(36, 12);
+            this.grafic1.Location = new System.Drawing.Point(3, 3);
             this.grafic1.Name = "grafic1";
+            series1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight;
+            series1.BackSecondaryColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(83)))), ((int)(((byte)(255)))));
+            series1.BorderWidth = 3;
             series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(88)))), ((int)(((byte)(127)))));
             series1.Legend = "Legend1";
+            series1.MarkerColor = System.Drawing.Color.MediumPurple;
+            series1.MarkerSize = 10;
             series1.Name = "Series1";
             this.grafic1.Series.Add(series1);
-            this.grafic1.Size = new System.Drawing.Size(679, 174);
+            this.grafic1.Size = new System.Drawing.Size(673, 186);
             this.grafic1.TabIndex = 0;
             this.grafic1.Text = "chart1";
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            title1.ForeColor = System.Drawing.Color.Gainsboro;
             title1.Name = "Title1";
             title1.Text = "Ingresos";
             this.grafic1.Titles.Add(title1);
             // 
             // graficpastel
             // 
+            this.graficpastel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            chartArea2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
             chartArea2.Name = "ChartArea1";
             this.graficpastel.ChartAreas.Add(chartArea2);
             this.graficpastel.Cursor = System.Windows.Forms.Cursors.Arrow;
+            legend2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            legend2.ForeColor = System.Drawing.Color.Gainsboro;
             legend2.Name = "Legend1";
             this.graficpastel.Legends.Add(legend2);
-            this.graficpastel.Location = new System.Drawing.Point(241, 192);
+            this.graficpastel.Location = new System.Drawing.Point(3, 3);
             this.graficpastel.Name = "graficpastel";
-            this.graficpastel.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            this.graficpastel.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.graficpastel.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(160)))), ((int)(((byte)(139))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(180)))), ((int)(((byte)(0))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(88)))), ((int)(((byte)(127))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(220)))), ((int)(((byte)(205))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(83)))), ((int)(((byte)(255))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(153)))), ((int)(((byte)(254)))))};
+            series2.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalRight;
+            series2.BackSecondaryColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(159)))), ((int)(((byte)(255)))));
+            series2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            series2.BorderWidth = 8;
             series2.ChartArea = "ChartArea1";
             series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             series2.Legend = "Legend1";
             series2.Name = "Series1";
             series2.YValuesPerPoint = 4;
             this.graficpastel.Series.Add(series2);
-            this.graficpastel.Size = new System.Drawing.Size(300, 300);
+            this.graficpastel.Size = new System.Drawing.Size(464, 287);
             this.graficpastel.TabIndex = 1;
             this.graficpastel.Text = "chart1";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.grafic1);
+            this.panel1.Location = new System.Drawing.Point(39, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(679, 192);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.panel2.Controls.Add(this.graficpastel);
+            this.panel2.Location = new System.Drawing.Point(126, 210);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(470, 291);
+            this.panel2.TabIndex = 3;
             // 
             // Analisis
             // 
@@ -86,14 +154,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
             this.ClientSize = new System.Drawing.Size(747, 513);
-            this.Controls.Add(this.graficpastel);
-            this.Controls.Add(this.grafic1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Analisis";
             this.Text = "Analisis";
             this.Load += new System.EventHandler(this.Analisis_Load);
             ((System.ComponentModel.ISupportInitialize)(this.grafic1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.graficpastel)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -102,5 +172,7 @@
 
         private System.Windows.Forms.DataVisualization.Charting.Chart grafic1;
         private System.Windows.Forms.DataVisualization.Charting.Chart graficpastel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
